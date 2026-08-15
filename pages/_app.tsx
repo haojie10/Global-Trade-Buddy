b0d50176b0fe140759b9b16d900e5a5200b4a03b
Jason Shen <haojie10@gmail.com>
2026-08-15 15:47:05 +0800

    feat(ui): 基于品牌 MGLogo 生成全套网站高清 Favicon 网站图标体系并全局注入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
-        <title>Market Graphic - 俯瞰全球市场</title>
+        <title>Market Graphic - 俯瞰全球市场结构</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head>
       {!isAdmin && <BackgroundGraph />}
       <Component {...pageProps} />
